6a874e722167d46d0a7a269dec60411d38ed29a1
Keunbae Park <ppotpo@gmail.com>
2022-03-09 22:27:12 -0500

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Git clone
+
+This extension will valet share a project in `~/code` and run valet commands in `./commands.json`.
+
+### Trigger
+
+Trigger this extension with `va `.
---
+++ __init__.py
@@ -0,0 +1,41 @@
+from albert import *
+import re
+from os import path
+import json
+
+__title__ = "Start"
+__version__ = "0.4.0"
+__triggers__ = "start "
+__authors__ = "pkboom"
+
+icon = "{}/icon.png".format(path.dirname(__file__))
+
+dir = path.dirname(path.abspath(__file__))
+
+commands = path.join(dir, 'commands.json')
+
+file = open(commands)
+commands = json.load(file)
+file.close()
+
+
+
+def handleQuery(query):
+    if not query.isTriggered or not query.isValid:
+        return
+    
+    print(' && '.join(commands))
+
+    items = []
+
+    items.append(Item(
+        id='start',
+        icon=icon,
+        text='start',
+        actions=[TermAction(
+            text='Start work', 
+            script=' && '.join(commands), 
+        )],
+    ))
+
+    return items
